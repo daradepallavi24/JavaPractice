31668afe334592498603a0629d45d0f3a9caf1c0
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-08-28 16:06:04 +0530

    Add files via upload

---
+++ DataTypeDemo.java
@@ -0,0 +1,88 @@
+package com.java.demo.intro;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		/*int marks=90;
+		
+		int age=80;
+		
+		if(marks>80)
+		{
+			System.out.println("Disctintion");
+		}
+		else if(marks>=60 && marks <=80)
+		{
+			System.out.println("First Class");
+		}
+		else 
+		{
+			System.out.println("you are pass");
+		}
+		
+		if(age>=18)
+		 {
+			 System.out.println("eligible for voting");
+		 }
+		 else
+		 {
+			 System.out.println("Not eligible for voting");
+		 }
+		
+		if(marks>80)
+		{
+			System.out.println("Disctintion");
+		}
+		else if(marks>=60 && marks <=80)
+		{
+			System.out.println("First Class");
+		}
+		else 
+		{
+			System.out.println("you are pass");
+		}
+		
+		if(marks>80)  // nested if
+		{
+			if(age>=18)
+			{
+			System.out.println("Eligible to persue for MBA");
+		}
+		}
+		
+ char grade='M';
+		 
+		 switch(grade) {
+        
+		 case 'A' :
+	            System.out.println("Excellent!"); 
+	            break; //stop
+	     case 'B' :
+	    	 System.out.println("good!"); 
+	    	 break;
+	     case 'C' :
+	    	 System.out.println("ok!"); 
+	    	 break;
+	    	 default:
+	    		 System.out.println("Invalid Grade");
+	    	 }
+	    	 
+	    int a = 5;
+		System.out.println(++a);          
+		 System.out.println(a);
+		 a++;          
+		 System.out.println(a);
+		 --a;          
+		 System.out.println(a);
+		 System.out.println(a--);
+		 */
+		 
+		 int a;
+			for(a=3;a<=10;a++)
+			{
+				System.out.println("Value of a=" + a);
+				}
+		 
+		}
+	}
+
