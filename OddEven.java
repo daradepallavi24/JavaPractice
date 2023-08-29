4d2fc53eb563ab056e893e617a92b5fe79bb0c6e
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-08-29 15:17:25 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+package com.java.demo.intro;
+
+public class OddEven {
+		 
+	    public static void main(String[] args)
+	    {
+	        int num = 10;
+	        if (num % 2 == 0)
+	        {
+	        	System.out.println("Entered Number is Even");
+	        }
+	        else 
+	        {
+	            System.out.println("Entered Number is Odd");
+	        }
+	    }
+	}
+
