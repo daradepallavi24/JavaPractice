9ec27982ed1c946da0d4003f3a0d03b00680ae9f
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-08-29 15:13:14 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+package com.java.demo.intro;
+public class Swap {
+	  public static void main(String[] args) {
+	    int x = 1;
+	    int y = 2;
+
+	    System.out.println("Before swapping: x = " + x + " y = " + y);
+
+	    int temp = x;
+	    x = y;
+	    y = temp;
+
+	    System.out.println("After swapping: x = " + x + " y = " + y);
+	    }
+	}
