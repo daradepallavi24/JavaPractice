495c531eb8b2a089785d72f9de17b3b8800dcf67
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-08-31 16:22:25 +0530

    Swap.java

--- Swap.java
+++ Swap.java
@@ -1,4 +1,21 @@
-package com.java.demo.intro;
+package com.demo;
+
+/*Given values: x = 1, y = 2
+
+1. Print: "Before swapping: x = 1 y = 2"
+
+2. Initialize temp = 1 (value of x).
+
+3. Assign y's value to x: x = 2
+   Assign temp's value (1) to y: y = 1
+
+4. Print: "After swapping: x = 2 y = 1"
+
+Final output:
+Before swapping: x = 1 y = 2
+After swapping: x = 2 y = 1
+*/
+
 public class Swap {
 	  public static void main(String[] args) {
 	    int x = 1;
