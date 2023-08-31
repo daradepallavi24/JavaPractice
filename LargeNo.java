a0923a2ba6e55028e51a1a911e5fb11124a4dd07
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-08-31 16:20:52 +0530

    LargeNo.java

--- LargeNo.java
+++ LargeNo.java
@@ -1,4 +1,13 @@
-package com.java.demo.intro;
+package com.demo;
+
+/*Given numbers: n1 = 4, n2 = 3, n3 = 5
+
+1. Compare n1 with n2 and n3:
+   (4 >= 3) is true and (4 >= 5) is false.
+   The first condition is true, so print: "4 is the largest number."
+
+Final output: 4 is the largest number.
+*/
 
 public class LargeNo {
 	public static void main(String[] args) {
