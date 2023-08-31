0894d017f554de7cca17a0690d27262823fdd048
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-08-31 16:21:59 +0530

    PositiveNegative.java

--- PositiveNegative.java
+++ PositiveNegative.java
@@ -1,3 +1,32 @@
+package com.demo;
+
+/*Given number: num = 912
+
+1. Check the condition: (num > 0)
+   912 is greater than 0, so print: "The number is positive."
+
+Final output: The number is positive.
+*/
+
+public class PositiveNegative {
+	public static void main(String[] args)   
+	{  
+	int num=912;  
+	if(num>0)  
+	{  
+	System.out.println("The number is positive.");  
+	}  
+	else if(num<0)  
+	{  
+	System.out.println("The number is negative.");  
+	}  
+	else  
+	{  
+	System.out.println("The number is zero.");  
+	}  
+	}  
+	}  
+
 package com.java.demo.intro;
 
 public class PositiveNegative {
