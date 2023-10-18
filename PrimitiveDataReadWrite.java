4098980759604ad107c82da4d3a95c84fa35f86c
daradepallavi24 <143394082+daradepallavi24@users.noreply.github.com>
2023-10-18 14:49:40 +0530

    Add files via upload

--- PrimitiveDataReadWrite.java
+++ PrimitiveDataReadWrite.java
@@ -1,41 +1,44 @@
-package packagecom.stremdemo;
-
 import java.io.*;
+/* 
+*The code begins, and the filePath is set to "primitiveData.txt."
+*It enters the try block and starts writing data to the file.
+*DataOutputStream is created to write data to the file.
+*It writes the integer 42, the double 3.14, and the boolean true to the file.
+*The DataOutputStream is closed.
+*Next, the code begins reading data from the same file.
+*DataInputStream is created to read data from the file.
+*The DataInputStream is closed.
+*The code then prints the data it read from the file:
+"Read int data: 42"
+"Read double data: 3.14"
+"Read boolean data: true"
+The program ends.*/
 
 public class PrimitiveDataReadWrite {
     public static void main(String[] args) {
-        // Define the file path
         String filePath = "primitiveData.txt";
 
         try {
-            // Create a DataOutputStream to write primitive data to the file
+            // Writing to the file
             DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(filePath));
-
-            // Write primitive data to the file
             dataOutputStream.writeInt(42);
             dataOutputStream.writeDouble(3.14);
             dataOutputStream.writeBoolean(true);
-
-            // Close the DataOutputStream
             dataOutputStream.close();
 
-            // Create a DataInputStream to read primitive data from the file
+            // Reading from the file
             DataInputStream dataInputStream = new DataInputStream(new FileInputStream(filePath));
-
-            // Read and display the primitive data from the file
             int intData = dataInputStream.readInt();
             double doubleData = dataInputStream.readDouble();
             boolean booleanData = dataInputStream.readBoolean();
+            dataInputStream.close();
 
+            // Printing the read data
             System.out.println("Read int data: " + intData);
             System.out.println("Read double data: " + doubleData);
             System.out.println("Read boolean data: " + booleanData);
-
-            // Close the DataInputStream
-            dataInputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
